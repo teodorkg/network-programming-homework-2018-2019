8f684f1569113a91b6dfc5678ce9b6dfa16803f2
tkgrozdanov <teodor.grozdanov@gmail.com>
2018-12-28 21:55:58 +0200

    add send image, send video and send file features

--- com/fmi/mpr/hw/chat/MulticastChatClient.java
+++ com/fmi/mpr/hw/chat/MulticastChatClient.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MulticastChatClient implements Runnable{
@@ -46,10 +49,8 @@ public class MulticastChatClient implements Runnable{
         while ((cmd = reader.readLine()) != null) {
             if ("sendText".equalsIgnoreCase(cmd)) {
                 sendUDPText(socketOut, "230.0.0.1", 8888);
-            } else if ("sendImage".equalsIgnoreCase(cmd)) {
-
-            } else if ("sendVideo".equalsIgnoreCase(cmd)) {
-
+            } else if ("sendImage".equalsIgnoreCase(cmd) || "sendVideo".equalsIgnoreCase(cmd) || "sendFile".equalsIgnoreCase(cmd)) {
+                sendUDPFile(socketOut, "230.0.0.1", 8888);
             } else if ("exit".equalsIgnoreCase(cmd) || socketOut.isClosed()) {
                 if ("exit".equalsIgnoreCase(cmd))
                     sendCloseNotificationToChatReceiver("230.0.0.1", socketIn.getLocalPort());
@@ -62,8 +63,10 @@ public class MulticastChatClient implements Runnable{
         }
     }
 
+
+
     private void sendCloseNotificationToChatReceiver(String ip, int port) throws IOException {
-        byte[] msg = "CLOSE_SOCKET".getBytes();
+        byte[] msg = "_CLOSE_SOCKET".getBytes();
         InetAddress to = InetAddress.getByName(ip);
         DatagramPacket packet = new DatagramPacket(msg, msg.length, to, port);
         socketOut.send(packet);
@@ -74,15 +77,59 @@ public class MulticastChatClient implements Runnable{
         }
     }
 
-    public static void sendUDPText(DatagramSocket socket, String ip, int port) throws IOException {
+    public static void sendUDPText(DatagramSocket socketOut, String ip, int port) throws IOException {
         Scanner sc = new Scanner(System.in);
         String line = sc.nextLine();
 
         InetAddress to = InetAddress.getByName(ip);
-        byte[] msg = line.getBytes();
+        byte[] msg = ("_TEXT " + line).getBytes();
 
         DatagramPacket packet = new DatagramPacket(msg, msg.length, to, port);
 
-        socket.send(packet);
+        socketOut.send(packet);
+    }
+
+    private void sendUDPFile(DatagramSocket socketOut, String ip, int port) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String url = sc.nextLine();
+        String fileName = url.substring(url.lastIndexOf("\\") + 1);
+        InetAddress to = InetAddress.getByName(ip);
+
+        byte[] msg = ("_TEXT sending " + fileName).getBytes();
+        DatagramPacket packet = new DatagramPacket(msg, msg.length, to, port);
+        socketOut.send(packet);
+
+        msg = ("_FILE " + fileName).getBytes();
+        packet = new DatagramPacket(msg, msg.length, to, port);
+        socketOut.send(packet);
+
+        int end;
+        byte[] file = fileToBytes(url);
+        if (file != null) {
+            for (int offset = 0; offset < file.length; offset += 1024) {
+                end = (file.length < offset + 1024) ? file.length : offset + 1024;
+                byte[] data = Arrays.copyOfRange(file, offset, end);
+                packet = new DatagramPacket(data, data.length, to, port);
+                socketOut.send(packet);
+            }
+        }
+        else {
+            System.out.println("Read file problem");
+        }
+
+        msg = ("_END").getBytes();
+        packet = new DatagramPacket(msg, msg.length, to, port);
+        socketOut.send(packet);
+    }
+
+    private byte[] fileToBytes(String url) {
+        byte[] bs;
+        try {
+            bs = Files.readAllBytes(Path.of(url));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return  null;
+        }
+        return bs;
     }
 }
--- com/fmi/mpr/hw/chat/MulticastChatReceiver.java
+++ com/fmi/mpr/hw/chat/MulticastChatReceiver.java
@@ -1,5 +1,7 @@
 package com.fmi.mpr.hw.chat;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.*;
 
@@ -12,11 +14,9 @@ public class MulticastChatReceiver implements Runnable{
         this.socketOut = socketOut;
     }
 
-    public void receiveUDPMessage(String ip, int port) throws IOException {
+    public void receiveUDP(String ip, int port) throws IOException {
 
         byte[] buffer = new byte[1024];
-
-
         InetAddress group = InetAddress.getByName(ip);
         socketIn.joinGroup(group);
         DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
@@ -24,27 +24,67 @@ public class MulticastChatReceiver implements Runnable{
             socketIn.receive(packet);
             String msg;
             msg = new String(packet.getData(), packet.getOffset(), packet.getLength());
+            String msgCmdPart;
+            msgCmdPart = (msg.indexOf(' ') != -1) ? msg.substring(0, msg.indexOf(' ')) : msg;
+            msg = msg.substring(msg.indexOf(' ') + 1);
             if (packet.getPort() == socketOut.getLocalPort()) {
-                if ("CLOSE_SOCKET".equals(msg)) {
+                //message from myself
+                if ("_CLOSE_SOCKET".equals(msgCmdPart)) {
                     socketIn.close();
                     socketOut.close();
                 }
-                else
+                else if ("_TEXT".equals(msgCmdPart)) {
                     System.out.println("You: " + msg);
+                }
             }
             else {
-                if ("CLOSE_SOCKET".equals(msg))
+                //message from other user
+                if ("_CLOSE_SOCKET".equals(msgCmdPart))
                     continue;
-                msg = new String(packet.getData(), packet.getOffset(), packet.getLength());
-                System.out.println(packet.getSocketAddress().toString() + ": " + msg);
+                else if ("_TEXT".equals(msgCmdPart))
+                    System.out.println(packet.getSocketAddress().toString() + ": " + msg);
+                else if ("_FILE".equals(msgCmdPart)) {
+                    String fileNameAndExt = msg;
+                    String fileName = fileNameAndExt.substring(0, fileNameAndExt.lastIndexOf('.'));
+                    String extension = fileNameAndExt.substring(fileNameAndExt.lastIndexOf('.'));
+                    fileName = socketOut.getLocalPort() + File.separator + fileName;
+                    File receivingFile = new File(fileName + extension);
+                    int i = 0;
+                    while (receivingFile.exists()) {
+                        i++;
+                        receivingFile = new File(fileName + "(" + i + ")" + extension);
+                    }
+                    if (i > 0)
+                        fileName = fileName + "(" + i + ")";
+                    receivingFile.getParentFile().mkdirs();
+                    receivingFile.createNewFile();
+                    FileOutputStream receiving = new FileOutputStream(receivingFile);
+                    receiveUDPFile(receiving);
+                    System.out.println("received: " + fileName + extension);
+                }
             }
         }
+    }
+
+    private void receiveUDPFile(FileOutputStream receiving) throws IOException {
+        byte[] buffer = new byte[1024];
+        String msg;
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        socketIn.receive(packet);
+        msg = new String(packet.getData(), packet.getOffset(), packet.getLength());
+        while (!"_END".equals(msg)) {
+            receiving.write(packet.getData());
+            socketIn.receive(packet);
+            msg = new String(packet.getData(), packet.getOffset(), packet.getLength());
+            receiving.flush();
+        }
+        receiving.close();
     }
 
     @Override
     public void run() {
         try {
-            receiveUDPMessage("230.0.0.1", 8888);
+            receiveUDP("230.0.0.1", 8888);
         } catch (IOException e) {
             e.printStackTrace();
         }
